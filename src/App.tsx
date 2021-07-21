693de5e821810266976c4ddae5a6745775e6f06e
Tetsuya Yamada <31818868+tetymd@users.noreply.github.com>
2021-07-21 19:14:05 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@material-ui/core";
+import { Header } from "./components";
+
+export default function App() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
---
+++ src/components/navbar/Header.tsx
@@ -0,0 +1,11 @@
+import { Box, AppBar, Toolbar, Typography } from "@material-ui/core";
+
+export const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h6">TECHI BLOG</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
